da081137f73920529eb094983691dc56ce584af0
Juan Yepes <yepes.j@hotmail.com>
2021-11-02 12:10:16 -0400

    simple docker test api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express')
+const cors = require('cors')
+const port = process.env.PORT || 3000
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = express()
+app.use(cors())
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/', (req,res) => {
+  res.send('🐳 Dockerrr is cool! 🐳')
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}/`)
+})
